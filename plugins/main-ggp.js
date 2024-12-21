0c15f4cc2f15d43d92f1d219303290eb97c72762
xeonser <167776975+xeonser@users.noreply.github.com>
2024-12-21 23:29:37 +0200

    Update main-ggp.js

--- plugins/main-ggp.js
+++ plugins/main-ggp.js
@@ -1,9 +1,9 @@
 let handler = async m => m.reply(`𝚂𝚄𝙿𝙿𝙾®𝚃 𝙶𝚁𝙾𝚄𝙿 𝙻𝙸𝙽𝙺
- ★☆⚡𝑷-𝑴𝑫⚡☆★
+ ★☆⚡𝑺𝑼𝑩𝒁𝑬𝑹𝑶-𝑴𝑫⚡☆★
 
-*${mssg.link}*: https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP
+*${mssg.link}*: https://chat.whatsapp.com/InsR5qk3cBsG2781A6uxcO
 
-*${mssg.link}*: https://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11
+*${mssg.link}*: https://whatsapp.com/channel/0029VagQEmB002T7MWo3Sj1D
 
 *OWNER*
 ${developer}
