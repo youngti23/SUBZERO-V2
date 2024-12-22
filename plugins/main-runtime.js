f229edd6cddc1b56b28576489dfb9114efe8379d
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-22 10:37:54 +0200

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -10,7 +10,7 @@ m.react('⏳')
       }) * 1000
     }
     let muptime = clockString(_muptime)
-    let str = `🟢sᴜʙᴢᴇʀᴏᴍᴅ ᴜᴘᴛɪᴍᴇ\n\n🎗️ʙᴏᴛ ᴀᴄᴛɪᴠᴇ ᴅᴜʀᴀᴛɪᴏɴ\n *${muptime}⏰*`
+    let str = `⭐sᴜʙᴢᴇʀᴏᴍᴅ ᴜᴘᴛɪᴍᴇ\n\n❄ʙᴏᴛ ᴀᴄᴛɪᴠᴇ ᴅᴜʀᴀᴛɪᴏɴ\n *${muptime}⏰*`
     conn.sendMessage(m.chat, {
       text: str,
       contextInfo: {
@@ -18,13 +18,13 @@ m.react('⏳')
       mentionedJid: [m.sender],
       isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363199257221654@newsletter',
+                newsletterJid: '120363304325601080@newsletter',
                 newsletterName: global.author,
                 serverMessageId: -1
             },
       forwardingScore: 999,
       externalAdReply: {
-      title: "🎗️ᴘʀɪɴᴄᴇ ᴍᴅ🎗️",
+      title: "❄ sᴜʙᴢᴇʀᴏ ᴍᴅ❄",
       body: "ʀᴜɴɪɴɢ sɪɴᴄᴇ",
       thumbnailUrl: pp,
       sourceUrl: '',
