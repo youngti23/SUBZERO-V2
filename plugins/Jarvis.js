d107f02f9adebfd7afb9d8091138403dda18e279
Frank Kaumba <engeezgamer@gmail.com>
2024-12-23 17:39:09 +0200

    Update Jarvis.js

--- plugins/Jarvis.js
+++ plugins/Jarvis.js
@@ -1,3 +1,19 @@
+/*╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺
+    ⭐ＰＲＯＪＥＣＴ ＮＡＭＥ:
+    ＳＵＢＺＥＲＯ  ＭＤ ᐯ2
+    
+    ⭐ＤＥＶＥＬＯＰＥＲＳ
+     ＭＲ ＦＲＡＮＫ  & ＦＲＡＮＫ
+     
+    ⭐ ＭＹ ＴＥＡＭ
+     ＸＥＲＯ ＣＯＤＥＲＳ
+     
+    ⭐ ＯＵＲ ＷＥＢＳＩＴＥ
+     https://github.com/mrfrank-ofc/SUBZERO-V2
+
+© Recoding This Script In Unacceptable⚠
+
+╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺*/
 import fetch from 'node-fetch';
 import gtts from 'node-gtts';
 import { readFileSync, unlinkSync } from 'fs';
